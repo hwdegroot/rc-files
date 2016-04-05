2d23a650593d6749b17d5c01fa79a4c35afd410f
Rik de Groot <rik.de.groot@mendix.com>
2016-04-05 16:11:08 +0200

    Change colors

--- .tmux-powerline/themes/default.sh
+++ .tmux-powerline/themes/default.sh
@@ -28,10 +28,12 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
     #"ifstat 30 255" \
     #"ifstat_sys 30 255" \
     #"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-    "wan_ip 24 255" \
-    "vcs_branch 29 88" \
+    "wan_ip 235 136" \
+    #"vcs_branch 29 88" \
+    "vcs_branch 24 250" \
     "vcs_compare 60 255" \
-    "vcs_staged 64 255" \
+    #"vcs_staged 64 255" \
+    "vcs_staged 29 255" \
     "vcs_modified 9 255" \
     "vcs_others 245 0" \
   )
