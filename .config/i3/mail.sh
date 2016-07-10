047d28102e7a842439fa3063119b94393fafc6b1
Rik de Groot <hwdgeroot@gmail.com>
2016-07-10 12:00:02 +0200

    Logoff script

--- .config/i3/mail.sh
+++ .config/i3/mail.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-USER=Your_Gmail_Username
-PASS=Your_Password
+source $HOME/.credentials/gmail
 
 COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>unknown number of</fullcount>"`
 COUNT=`echo "$COUNT" | grep -oPm1 "(?<=<fullcount>)[^<]+" `
