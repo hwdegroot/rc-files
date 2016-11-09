bf971e8e4191d60d214ca15cabf32bfe7f1a4638
Rik de Groot <rik.de.groot@mendix.com>
2016-11-09 20:22:20 +0100

    Logos

--- .config/i3/i3blocks/mail.sh
+++ .config/i3/i3blocks/mail.sh
@@ -1,19 +1,17 @@
+#!/bin/bash
+
 . ${1}
 USER=$USERNAME
 PASS=$PASSWORD
 
 COUNT=`curl -su $USER:$PASS https://mail.google.com/mail/feed/atom || echo "<fullcount>unknown number of</fullcount>"`
 COUNT=`echo "$COUNT" | grep -oPm1 "(?<=<fullcount>)[^<]+" `
-echo $COUNT
-if [ "$COUNT" != "0" ]; then
-   if [ "$COUNT" = "1" ];then
-     WORD="mail";
-   else
-      WORD="mails";
-   fi
-fi
-echo "$COUNT unread $WORD"
+WORD="mail"
 COLOR="#d5d5d5"
-[[ $((COUNT)) -gt 0 ]] && COLOR="#5af665"
+[[ $((COUNT)) -gt 1 ]] && WORD="mails";
+[[ $((COUNT)) -gt 0 ]] && COLOR="#aa0000"
+
+echo $COUNT
+echo "$COUNT unread $WORD"
 echo $COLOR
 
